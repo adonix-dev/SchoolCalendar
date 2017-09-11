b54549c59e5a17239bed2ba63e9ba8a13014ed32
Adonix <31833518+adonix-dev@users.noreply.github.com>
2017-09-11 14:13:34 +0200

    project init
    project init

---
+++ core/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define("DOMAIN_NAME","adonix.org"); 		// DOMAIN NAME
+define("SITE_URL","http://adonix.org/projects/AdonixSchoolCalendar"); // SITE URL
+define("PRIVATE_API_KEY", 'KqJ6-4@uG-h_ht-9s2t-gio9-6@56-fg64-dDFQ-V6!5-to_9-9r97-41@z-s6e5-5Wpa-X_6H-Kq!B-OB43-TS@Y');
+define("DB_HOST","localhost"); 				// Database Hostname
+define("DB_USER","null"); 					// Database Username
+define("DB_PASS","null"); 					// Database Password
+define("DB_NAME","null"); 					// Database Name
+define("CAPTCHA_PUBLIC_KEY",'null'); 		//www.recaptcha.net
+define("CAPTCHA_PRIVATE_KEY",'null'); 		//www.recaptcha.net
+define("FROM_MAIL",'contact@adonix.com'); 	// Sender email address
+define("FROM_NAME",'Adonix'); 				// Sender Name
